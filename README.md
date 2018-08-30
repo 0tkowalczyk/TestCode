1db880ed716412c6862991b85582dfa8feab1a43
0tkowalczyk <42749742+0tkowalczyk@users.noreply.github.com>
2018-08-30 13:57:32 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TestCode
-Day 2 Finished 
+We practice typing code into our editor and uploading it to Github using the website.
+
+## Directions
+Read the comments in each file and complete the programs.
---
+++ vars.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = -25;
+//create two more variables c and d and give them whatever number you want.
+let c = 4;
+let d = 5;
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name = "Tomasz Kowalczyk";
+//add string convo and name together.
+const greeting = convo + name;
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2 = "See you ";
+let convo3 = "tomorrow ";
+const farawell = convo2 + convo3 + name
+console.log(farawell);
